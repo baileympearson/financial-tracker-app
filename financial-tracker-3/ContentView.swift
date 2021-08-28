505a8b2ea88adc889dfcb74a3b9569dae9092642
Bailey Pearson <bailey.pearson@vanderbilt.edu>
2021-08-28 16:30:44 -0400

    Initial Commit

---
+++ financial-tracker-3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  financial-tracker-3
+//
+//  Created by Bailey Pearson on 8/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
